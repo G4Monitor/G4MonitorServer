e3a7c5497df539bab198ec0bb7de3bc153618b41
RobinBilly <rbilly.mail@gmail.com>
2015-04-09 18:40:56 +0200

    pourcentage alerte

--- js/device-details.js
+++ js/device-details.js
@@ -90,10 +90,10 @@ $(document).ready(function() {
 			});
 			$('#percentUsedRAM').text(data['percent_used_ram']);
 
-			if(data['percent_used_ram'] < 20) {
+			if(data['percent_used_ram'] < 50) {
 				$('#percentUsedRAMParent').addClass("success");
 			}
-			else if(data['percent_used_ram'] >= 30) {
+			else if(data['percent_used_ram'] >= 80) {
 				setTimeout(function(){
 					$('#percentUsedRAMParent').addClass("alert");
 					var requestSendAlertRAM = $.ajax({
