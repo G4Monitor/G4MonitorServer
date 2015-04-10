04943a07c7172a5ff4b5088af1818691fcaef859
Rowmain <dalleau.romain@gmail.com>
2015-04-10 13:31:25 +0200

    modif

--- send-alert.php
+++ send-alert.php
@@ -3,8 +3,7 @@ include "conf.php";
 
 $config = new Config;
 
-echo $config->email_adress_to_inform();
-die();
+
 	date_default_timezone_set('Europe/Paris');
 
 	$deviceMac = substr(trim($_POST['deviceMac'], '\n'),0,17);
